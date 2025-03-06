d963b310824dd4bdae94ae8e12f2236c773e5a8e
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-03-05 21:37:36 -0300

    Fin de la Clase 01 | JS

---
+++ clase_01/app.js
@@ -0,0 +1,32 @@
+// Variables 
+
+var nombre;
+nombre = `Eduardo`; // String o Cadena de Texto ("",'',``)
+
+var edad = 29; // Number, y JS no diferencia en diferentes tipos de numero
+var rating = 3.14;
+
+var esPortenio = true; // Boolean o Booleano (true o false)
+var estaLloviendo= false;
+
+undefined 
+null
+NaN // not as number
+
+var confusion = "A" + 1; // NaN
+
+var altura = "250ml"
+var vaso = {altura,diametro:"5cm"}
+
+var informacion = ["Eduardo","eduardo@gmail.com",true,29] 
+
+
+// Funciones/Propiedades del Navegador
+
+// console.log(nombre)
+// console.table(informacion)
+// console.error("Error")
+// console.warn("Peligro")
+// console.log(prompt("Ingresa tu edad?"))
+// console.log(confirm("Eres mayor de edad"))
+// alert("Bienvenido a JS")
