35017b7c45556855ac96882d12d79f1050b5a23a
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-05-28 20:58:02 -0300

    End | Clase 23 | Timer & Promesas

--- clase_23/modules/promises.js
+++ clase_23/modules/promises.js
@@ -1,1 +1,16 @@
 // Promesas || new Promise
+
+// const data = async () => {};
+
+const loader = (sucess = true) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve({ msg: "Hola desde una promesa" }), 1000);
+    if (!sucess) {
+      reject(new Error("Promesa rechazada, no tenes los datos"));
+    }
+  });
+};
+
+loader(false)
+  .then((res) => console.log(res))
+  .catch((e) => console.log(e.message));
--- clase_23/modules/timer.js
+++ clase_23/modules/timer.js
@@ -1,3 +1,57 @@
-// Timer
 // setTimeout
 // setInterval | clearInterval
+// console.log("Hola");
+// setTimeout(() => console.log("Nucba"), 1000);
+// console.log("Bienvenido a");
+// let contador = 0;
+// let intervalo = setInterval(() => {
+//   contador++;
+//   console.log("Valor:", contador);
+//   if (contador == 20) {
+//     clearInterval(intervalo);
+//   }
+// }, 1000);
+
+// Timer
+const formTimer = document.querySelector("#timer");
+const setting = document.querySelector("#timer > fieldset");
+const outTime = document.querySelector("#time");
+const inputMin = document.querySelector("#minutos");
+const inputSec = document.querySelector("#segundos");
+const btnStart = document.querySelector("#start");
+const btnStop = document.querySelector("#stop");
+let time = 0;
+let intervalo = null;
+btnStart.addEventListener("click", () => {
+  let mins = Number(inputMin.value) * 60 * 1000;
+  let secs = Number(inputSec.value) * 1000;
+  time = mins + secs;
+  setting.classList.remove("active");
+  outTime.classList.remove("active");
+  outTime.classList.add("active");
+  intervalo = setInterval(() => {
+    time -= 1000;
+    let template = null;
+    if (time < 60000) {
+      template = `${time / 1000} segundos`;
+    } else {
+      template = `${Math.floor(time / 60000)
+        .toString()
+        .padStart(2, "0")} minutos`;
+    }
+    outTime.innerHTML = `${template}`;
+    if (time == 0) {
+      clearInterval(intervalo);
+      setting.classList.remove("active");
+      outTime.classList.remove("active");
+      setting.classList.add("active");
+    }
+  }, 1000);
+});
+btnStop.addEventListener("click", () => {
+  clearInterval(intervalo);
+  setting.classList.remove("active");
+  outTime.classList.remove("active");
+  setting.classList.add("active");
+  time = 0;
+});
