5d3301e1313a0059cde630444c3c1b90687675bb
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-06-02 20:38:47 -0300

    End | Clase 24

--- clase_24/app.js
+++ clase_24/app.js
@@ -1,1 +1,31 @@
-const API_URL = "https://pokeapi.co/api/v2/";
+const API_URL = "https://pokeapi.co/api/v2";
+const getPoke = async (id = null) => {
+  let url = `${API_URL}/pokemon/`;
+  if (id) {
+    url += id;
+  }
+  let respuesta = await fetch(url);
+  return await respuesta.json();
+};
+getPoke().then((pokemones) => console.log(pokemones));
+
+const DATA_URL = "/data.json";
+let loading = true;
+const getProductos = async () => {
+  try {
+    console.log(loading);
+    let respuesta = await fetch(DATA_URL, {
+      method: "GET",
+      //   headers:{},
+      //   data:{}
+    });
+    return await respuesta.json();
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    loading = false;
+    console.log(loading);
+  }
+};
+
+// getProductos().then((productos) => console.log(productos));
