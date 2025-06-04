371b69f79bbe8d5143c325f8ca31b3ef15367e15
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-06-04 20:33:30 -0300

    End  | Clase 25

--- clase_25_NWeather/app.js
+++ clase_25_NWeather/app.js
@@ -16,14 +16,46 @@ let loading = false;
 let city = null;
 
 // Funciones
-const obtenerCoodenadas = async () => {};
-const obtenerClima = async () => {};
-const renderClima = (clima) => {
+const obtenerCoodenadas = async (ciudad) => {
+  const data = {
+    q: ciudad,
+    limit: 1,
+    appid: API_KEY,
+  };
+  const params = new URLSearchParams(data);
+  let url = `${API_URL}${GEO_ENDPOINT}?${params.toString()}`;
+  const respuesta = await fetch(url);
+  const coordenadas = await respuesta.json();
+  return { latitud: coordenadas[0].lat, longitud: coordenadas[0].lon };
+};
+const obtenerClima = async (latitud, longitud) => {
+  const data = {
+    lat: latitud,
+    lon: longitud,
+    units: "metric",
+    lang: "es",
+    appid: API_KEY,
+  };
+  const params = new URLSearchParams(data);
+  let url = `${API_URL}${WEATHER_ENDPOINT}?${params.toString()}`;
+  const respuesta = await fetch(url);
+  const clima = await respuesta.json();
+  return clima;
+};
+const renderClima = (cuidad, clima) => {
   weatheContainer.innerHTML = null;
-  const { weather, main, name } = clima;
-  let template = `<dl id="data"><dt>Buenos Aires</dt><dd>nubes</dd><dd>17°<span>17° ST</span></dd></dl>`;
-  template += `<picture id="icono"><img src="https://openweathermap.org/img/wn/04d@4x.png" alt="icono de clima nubes"/></picture>`;
-  template += `<ul id="extra"> <li>Max: 18º</li> <li>Min: 16º</li> <li>77% Humedad</li> </ul>`;
+  const { weather, main } = clima;
+  let template = `<dl id="data"><dt>${cuidad}</dt><dd>${
+    weather[0].description
+  }</dd><dd>${Math.round(main.temp)}°<span>${Math.round(
+    main.feels_like
+  )}° ST</span></dd></dl>`;
+  template += `<picture id="icono"><img src="${ICON_URL}${weather[0].icon}@4x.png" alt="icono de clima ${weather[0].description}"/></picture>`;
+  template += `<ul id="extra"> <li>Max:${Math.round(
+    main.temp_max
+  )}º</li> <li>Min: ${Math.round(main.temp_min)}º</li> <li>${
+    main.humidity
+  }% Humedad</li> </ul>`;
   weatheContainer.innerHTML = template;
   return;
 };
@@ -31,19 +63,26 @@ const renderClima = (clima) => {
 const validarCity = () => {
   let value = cityInput.value.trim();
   let success = true;
-  feedCity.innerHTML = "";
+  feedCity.innerHTML = null;
   feedCity.classList.remove("invalid");
   if (value.length < 4) {
     success = false;
-    feedCity.innerHTML = "Completar el nombre de la ciudad";
     feedCity.classList.add("invalid");
+    feedCity.innerHTML = "Completa el nombre de la Cuidad";
+  } else {
+    feedCity.innerHTML = `Así está el clima en ${value}, ¿Querés ver el clima de otra ciudad?`;
   }
   return success;
 };
 
 // Eventos
 
-cityInput.addEventListener("input", () => {});
-searchForm.addEventListener("submit", (e) => {
+cityInput.addEventListener("input", validarCity);
+searchForm.addEventListener("submit", async (e) => {
   e.preventDefault();
+  const coordenadas = await obtenerCoodenadas(cityInput.value.trim());
+  const clima = await obtenerClima(coordenadas.latitud, coordenadas.longitud);
+  renderClima(cityInput.value.trim(), clima);
 });
+
+// console.log(Object.fromEntries(new URLSearchParams(location.search).entries()));
