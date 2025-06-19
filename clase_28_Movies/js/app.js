a67e47194b22a7d53f67be4a2bc0b24bb9633925
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-06-18 21:09:55 -0300

    End | Clase 28

--- clase_28_Movies/js/app.js
+++ clase_28_Movies/js/app.js
@@ -1,4 +1,11 @@
-import { IMAGE, TOPRATED, TRENDING, UPCOMING, LIMIT } from "./constants.js";
+import {
+  KEY,
+  IMAGE,
+  TOPRATED,
+  TRENDING,
+  UPCOMING,
+  LIMIT,
+} from "./constants.js";
 // Estados Globales
 var currentPage = 0;
 var totalCount = 12;
@@ -14,7 +21,17 @@ const outPage = document.querySelector("#outPage");
 // Metodos
 const getData = async () => {
   try {
-    return [];
+    const search = new URLSearchParams({
+      api_key: KEY,
+      language: "es_AR",
+      page: currentPage + 1,
+    });
+    const res = await fetch(`${currentUrl}${search.toString()}`);
+    const data = await res.json();
+    totalCount = data.total_pages;
+    // let start = currentPage * LIMIT;
+    // let end = start + LIMIT;
+    return data.results;
   } catch (error) {
     throw new Error(error.message);
   }
@@ -22,15 +39,58 @@ const getData = async () => {
 const renderMovies = async () => {
   try {
     let movies = await getData();
-    console.log(movies);
+    list.innerHTML = null;
+    outPage.innerHTML = `${currentPage + 1}`;
+    movies.forEach((movie) => cardMovie(movie));
+    return;
   } catch (error) {
     throw new Error(error.message);
   }
 };
-const cardMovie = (movie) => {};
-const nextPage = () => {};
-const prevPage = () => {};
-const setFilter = (filter) => {};
+const cardMovie = (movie) => {
+  const { title, popularity, poster_path, release_date } = movie;
+  let element = document.createElement("li");
+  let template = ` <img loading="lazy" src="${IMAGE}${poster_path}" alt="Poster ${title}"/>`;
+  template += `<p>${Math.round(popularity)}% de popularidad</p>`;
+  template += `<dl><dt>${title}</dt><dd>Fecha de estreno: ${new Date(
+    release_date
+  ).toLocaleDateString("es")}</dd></dl>`;
+  element.innerHTML = template;
+  list.append(element);
+  return;
+};
+const nextPage = () => {
+  currentPage += 1;
+  let maxPage = Math.floor(totalCount / LIMIT);
+  if (currentPage >= 1) {
+    btnPrev.removeAttribute("disabled");
+  }
+  if (currentPage < maxPage) {
+    return renderMovies();
+  }
+  if (currentPage == maxPage) {
+    btnNext.setAttribute("disabled", "true");
+  }
+};
+const prevPage = () => {
+  currentPage -= 1;
+  if (currentPage <= 0) {
+    currentPage = 0;
+    btnPrev.setAttribute("disabled", "true");
+  }
+  return renderMovies();
+};
+const setFilter = (filter) => {
+  let value = filter.value;
+  let collections = {
+    top_rated: TOPRATED,
+    trending: TRENDING,
+    upcoming: UPCOMING,
+  };
+  currentUrl = collections[value];
+  currentPage = 0;
+  return renderMovies();
+};
 
 // Eventos
 btnNext.addEventListener("click", nextPage);
--- clase_28_Movies/js/constants.js
+++ clase_28_Movies/js/constants.js
@@ -1,7 +1,8 @@
-const KEY = "208a7cecc8d4ed65ea73d6abac17b319";
+export const KEY = "208a7cecc8d4ed65ea73d6abac17b319";
 const API = "https://api.themoviedb.org";
+
 export const IMAGE = "https://image.tmdb.org/t/p/original";
-export const TRENDING = `${API}/3/trending/movie/week?api_key=${KEY}&language=es_AR`;
-export const TOPRATED = `${API}/3/movie/top_rated?api_key=${KEY}&language=es_AR`;
-export const UPCOMING = `${API}/3/movie/upcoming?api_key=${KEY}&language=es_AR`;
+export const TRENDING = `${API}/3/trending/movie/week?`;
+export const TOPRATED = `${API}/3/movie/top_rated?`;
+export const UPCOMING = `${API}/3/movie/upcoming?`;
 export const LIMIT = 4;
